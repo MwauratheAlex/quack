ead961d59ef2c2fbd33ad7af4cfcb61c0b72a8f7
Mbugua <mwaurathealex@gmail.com>
2024-01-07 15:05:14 +0300

    profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from '@clerk/nextjs'
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
@@ -9,6 +10,11 @@ import { Toaster } from "react-hot-toast";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Quack</title>
+        <meta name="description" content="🧠" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center"/>
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,28 +1,16 @@
 // here we are in the server
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 import {
   createTRPCRouter,
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImgUrl: user.imageUrl,
-    externalUserName:
-      user.externalAccounts.find((externalAccount) => {
-        externalAccount.provider === "oauth_github";
-      })?.username ?? null,
-  };
-};
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
